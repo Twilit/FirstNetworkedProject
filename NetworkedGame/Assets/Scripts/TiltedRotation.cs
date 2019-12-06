f3b4bdde88bd8a8f36edd8c692120ee3b097805a
Twilit <kinlongsha@hotmail.co.uk>
2019-12-06 15:38:31 +0000

    start

---
+++ NetworkedGame/Assets/Scripts/TiltedRotation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltedRotation : MonoBehaviour
+{
+
+    public float faceDir;
+    public Quaternion axisTilt;
+    public Camera cam;
+
+    void Start()
+    {
+        axisTilt = transform.localRotation;
+    }
+
+    void Update()
+    {
+        //transform.localRotation = Quaternion.Euler(0, faceDir, 0) * axisTilt;
+
+        //transform.Rotate(0, faceDir, 0);
+        //transform.RotateAroundLocal(transform.up,faceDir);
+
+        transform.rotation = Quaternion.Euler( GetMousePosDir(transform.position));
+    }
+
+    Vector2 GetMousePosDir(Vector3 currentPos)
+    {
+        Vector3 mousPos = Input.mousePosition;
+        mousPos = cam.ScreenToViewportPoint(mousPos);
+
+        Vector2 dir = currentPos - mousPos;
+
+        return dir;
+    }
+}
