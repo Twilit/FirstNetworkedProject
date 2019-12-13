50f8ff04d2526f3e2b4112acc24b2b241a8d20f9
Twilit <kinlongsha@hotmail.co.uk>
2019-12-13 15:27:47 +0000

    something

---
+++ NetworkedGame/Assets/Scripts/PlayerPresenceScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerPresenceScript : NetworkBehaviour
+{
+    public GameObject playerUnit;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        CmdSpawnMyUnit();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    [Command]
+    void CmdSpawnMyUnit()
+    {
+        GameObject go = Instantiate(playerUnit);
+
+        //go.GetComponent<NetworkIdentity>().AssignClientAuthority(connectionToClient);
+
+        NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+    }
+}
---
+++ NetworkedGame/Assets/Scripts/PlayerUnitScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerUnitScript : NetworkBehaviour
+{
+    float h;
+    public float hSpeed;
+    Rigidbody2D rb2D;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        h = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb2D.velocity = new Vector2(h * hSpeed * Time.deltaTime, rb2D.velocity.y);
+    }
+}
